523752b93d3c15165ec2a16c880c181fdfdff3b8
Joshua <joshuaodunbaku@gmail.com>
2026-04-06 21:00:16 +0100

    Initial commit - Pharmacy frontend project

---
+++ src/assets/images.js
@@ -0,0 +1,38 @@
+const IMAGES = {
+    // Home page
+    logo: new URL("./imgs/truetouch-logo.png", import.meta.url).href,
+    hero1: new URL("./imgs/hero-1.jpg", import.meta.url).href,
+    hero2: new URL("./imgs/hero-2.jpg", import.meta.url).href,
+    hero3: new URL("./imgs/hero-3.jpg", import.meta.url).href,
+    image01: new URL("./imgs/image-01.jpg", import.meta.url).href,
+    image02: new URL("./imgs/image-02.jpg", import.meta.url).href,
+    image06: new URL("./imgs/image-06.jpg", import.meta.url).href,
+    image08: new URL("./imgs/image-08.jpg", import.meta.url).href,
+    image10: new URL("./imgs/image-10.jpg", import.meta.url).href,
+    image14: new URL("./imgs/image-14.jpg", import.meta.url).href,
+    image35: new URL("./imgs/image-35.jpg", import.meta.url).href,
+    image43: new URL("./imgs/image-43.jpg", import.meta.url).href,
+    image48: new URL("./imgs/image-48.jpg", import.meta.url).href,
+    image54: new URL("./imgs/image-54.jpg", import.meta.url).href,
+    image60: new URL("./imgs/image-60.jpg", import.meta.url).href,
+    image61: new URL("./imgs/image-61.jpg", import.meta.url).href,
+    image62: new URL("./imgs/image-62.jpg", import.meta.url).href,
+    image68: new URL("./imgs/image-68.jpg", import.meta.url).href,
+
+    ////////////// SVGs
+    // svg_fans_SECONDARY: new URL("./Svg/undraw_fans_SECONDARY.svg", import.meta.url).href,
+    // svg_fans: new URL("./Svg/undraw_fans.svg", import.meta.url).href,
+    // svg_golf_SECONDARY: new URL("./Svg/undraw_golf_SECONDARY.svg",import.meta.url ).href,
+    // svg_golf: new URL("./Svg/undraw_golf.svg", import.meta.url).href,
+    // svg_idea: new URL("./Svg/undraw_idea.svg", import.meta.url).href,
+    // svg_online_community_SECONDARY: new URL("./Svg/undraw_online-community_SECONDARY.svg",import.meta.url).href,
+    // svg_playing_golf_SECONDARY: new URL("./Svg/undraw_playing-golf_SECONDARY.svg", import.meta.url).href,
+    // svg_playing_golf: new URL("./Svg/undraw_playing-golf.svg", import.meta.url).href,
+    // svg_team_SECONDARY: new URL("./Svg/undraw_team_SECONDARY.svg", import.meta.url ).href,
+    // svg_team: new URL("./Svg/undraw_team.svg", import.meta.url).href,
+    // golf: new URL("./Svg/golf-svgrepo-com.svg", import.meta.url).href,
+    // svg_user: new URL("./Svg/user-svgrepo-com.svg", import.meta.url).href,
+    // subscribe: new URL("./Svg/subscribe.svg", import.meta.url).href,
+};
+
+export default IMAGES;
